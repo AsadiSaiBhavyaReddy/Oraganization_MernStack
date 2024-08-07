b96db4cb9149f9875dc6348514749f547bf44e23
AsadiSaiBhavyaReddy <145554222+AsadiSaiBhavyaReddy@users.noreply.github.com>
2024-08-07 21:37:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Changed Switch to Routes
+
+import Appbar from './components/Appbar';
+import Entry from './components/Entry';
+import Home from './components/Home';
+import Admission from './components/Admission';
+import Student from './components/Student';
+
+import Faculty from './components/Faculty';
+import ViewData from './components/ViewData';
+import Facultydata from './components/Facultydata';
+
+function App({ store }) {
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-body">
+          <Appbar store={store} />
+          <Routes> {/* Changed Switch to Routes */}
+           
+           
+            <Route path="/entry" element={<Entry />} />
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/view" element={<ViewData />} />
+
+            <Route path="/faculty" element={<Faculty />} />
+            <Route path="/facultydata" element={<Facultydata />} />
+
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/admission" element={<Admission />} />
+            <Route path="/student" element={<Student />} />
+          {/* Changed exact to element */}
+          </Routes> {/* Changed Switch to Routes */}
+        
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
